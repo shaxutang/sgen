ae154bcb6df73b2e41a34e5d0f7c5ea8bb247fc6
shaxutang <olaysunju@163.com>
2023-12-07 18:33:40 +0800

    remove: rm rest types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -6,32 +6,3 @@ export type Sgenrc = Partial<{
   workspace: string;
   [prop: string]: any;
 }>;
-
-export type MergeSgenrc = {
-  cwd: Sgenrc;
-  os: Sgenrc;
-  merge: Sgenrc;
-};
-
-export type File = {
-  name: string;
-  path: string;
-  size: number;
-  type: "file";
-  extension?: string;
-};
-
-export type Folder = {
-  name: string;
-  path: string;
-  size: number;
-  type: "directory";
-  children: MabeFile[];
-};
-
-export type MabeFile = Folder | File;
-
-export type MergeDirTree = {
-  sgenDirTree: Folder;
-  workspaceDirTree: Folder;
-};
